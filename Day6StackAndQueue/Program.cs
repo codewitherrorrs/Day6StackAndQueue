5d9105ff53539b28c56635ff0223f95e1ed42de7
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-15 12:35:38 +0530

    Add : Add Element in Queue Using LinkedList Technic

---
+++ Day6StackAndQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day6StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            //For Adding Element in queue.....
+            Queue queue = new Queue();
+            queue.Enqueue(20);
+            queue.Enqueue(10);
+            queue.Enqueue(40);
+            queue.Enqueue(54);
+            //For Showing / Display the queue.
+            queue.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day6StackAndQueue/Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6StackAndQueue
+{
+    public class Queue
+    {
+        Node head = null;
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+
+            }
+            //Console.WriteLine(node.data);
+
+        }
+
+        public void DequeueMethod()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                this.head = this.head.next;
+                Console.WriteLine("first element removed!!");
+            }
+
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
