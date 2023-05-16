e9f576d092c4b8adb771b864f9e56ee242033318
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-16 11:37:59 +0530

    Add : Stack Method

--- Day6StackAndQueue/Program.cs
+++ Day6StackAndQueue/Program.cs
@@ -5,20 +5,22 @@
         public static void Main(string[] args) 
         {
             //For Adding Element in queue.....
-            Queue queue = new Queue();
-            queue.Enqueue(20);
-            queue.Enqueue(10);
-            queue.Enqueue(40);
-            queue.Enqueue(54);
-            queue.Peek();
-            Console.WriteLine("Original Queue : ");
-            queue.Display();
+            //Queue queue = new Queue();
+            //queue.Enqueue(20);
+            //queue.Enqueue(10);
+            //queue.Enqueue(40);
+            //queue.Enqueue(54);
+            //queue.Peek();
+            //Console.WriteLine("Original Queue : ");
+            //queue.Display();
             //Remove the Element from Queue......
-            queue.Dequeue();
+            //queue.Dequeue();
             //For Find the Peek.
-            queue.Peek();
+            //queue.Peek();
             //For Showing / Display the queue.
-            queue.Display();
+            //queue.Display();
+            StackMethod stack = new StackMethod();
+            stack.MethodStack();
             Console.ReadKey();
         }
     }
---
+++ Day6StackAndQueue/StackMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6StackAndQueue
+{
+    public class StackMethod
+    {
+        public void MethodStack()
+        {
+            Stack stack = new Stack();
+            stack.Push(10);
+            stack.Push(11);
+            stack.Push(20);
+            stack.Push(30);
+            Console.WriteLine("Push element in stack :"); //=>30,20,11,10
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Pop element :" + stack.Pop());
+            Console.WriteLine("Peek element :" + stack.Peek());
+        }
+    }
+}
